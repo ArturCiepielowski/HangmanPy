d5394336c2f43ab77cfd6a437a6a6285cd75496b
Artur Ciepielowski <artciepi@gmail.com>
2022-02-07 18:53:41 +0100

    Testing new for loop

--- hangman.py
+++ hangman.py
@@ -4,20 +4,25 @@ word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = word_list[random.randint(0, 2)]
 
-guess = input("Guess a letter\n").lower()
-
-print(chosen_word)
-
 display = []
 for y in range(len(chosen_word)):
     display.append("_")
     # print(y)
+
+print(display)
+
+for letter in display:
+    if letter == "_":
+
+
+
+
+guess = input("Guess a letter\n").lower()
+
+
 
 
 for x, z in enumerate(chosen_word):
     # print(x)
     if z == guess:
         display[x] = guess
-
-
-print(display)
