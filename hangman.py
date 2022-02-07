68bda3293901d9e208fdb3b65de11cca37d424e5
Artur Ciepielowski <artciepi@gmail.com>
2022-02-07 23:07:22 +0100

    Added while loop

--- hangman.py
+++ hangman.py
@@ -12,16 +12,20 @@ display = []
 for _ in range(word_length):
     display += "_"
 
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-guess = input("Guess a letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(
+            f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}"
+        )
+        if letter == guess:
+            display[position] = letter
 
+    print(display)
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    print(
-        f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}"
-    )
-    if letter == guess:
-        display[position] = letter
-
-print(display)
+    if "_" not in display:
+        endof_of_game = True
+        print("You win.")
